5e62c63f6059627be07e8aec435deecad7ecdd6e
MatiGiraudo <matigiraudo97@hotmail.com>
2026-08-11 12:14:41 -0300

    refactor: update vite configuration to use mode parameter for dynamic base path resolution

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Grupo-KLH-v2/' : '/',
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      },
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/Grupo-KLH-v2/' : '/',
+    server: {
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true
+        },
+        '/uploads': {
+          target: 'http://localhost:5000',
+          changeOrigin: true
+        }
       }
     }
   }
